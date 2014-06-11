d5a1dd0958581f38c4b0dc571de1d86be62eca05
Karolienvh <vanherpkarolien@gmail.com>
2014-06-11 22:40:36 +0200

    Start applicatie
    basisstructuur en functies

--- www/js/index.js
+++ www/js/index.js
@@ -47,3 +47,14 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+(function($) {
+    "use strict";
+    $(document).ready(function() {
+        $('#access').on('touchstart click', '.skip-link', function(event) {
+            $(this).toggleClass('focus');
+            $($(this).attr('href')).toggleClass('target');
+            event.preventDefault();
+        }).find('.skip-link').append('<span>'+$('#menu .active').text()+'</span>');
+    });
+})(jQuery);
